3b076fd3ce6c26bc2b699e76bbf5a69da9772643
Carlos Diaz <carlos9559@gmail.com>
2023-06-08 09:05:20 -0500

    Count text area

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { len: 0 };
+  }
+
+  count = (event) => {
+    this.setState({ len: event.target.value.length });
+  };
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.count}></textarea>
+        <div className="counter">{this.state.len}</div>
       </div>
     );
   }
